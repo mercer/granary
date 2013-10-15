66febcb36f26e90f4a5b84508be3308cf0b0fd35
Radu Cirstoiu <radu.cirstoiu@cegeka.com>
2013-10-15 11:40:13 +0300

    vali, radu project skeleton

---
+++ ginkgo-seed/spa/app/js/controllers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('rechtwijzerAdmin.controllers', [])
+    .controller('HomeCtrl', [function () {
+    }])
+    .controller('UsersCtrl', [function () {
+    }])
+    .controller('UserCtrl', [function () {
+    }])
+;
---
+++ ginkgo-seed/spa/test/e2e/scenarios.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('rechtwijzer admin app', function () {
+
+    beforeEach(function () {
+        browser().navigateTo('../../app/index.html');
+    });
+
+
+    it('should automatically redirect to / when location hash/fragment is empty', function () {
+        expect(browser().location().url()).toBe("/");
+    });
+
+    describe('home', function () {
+
+        beforeEach(function () {
+            browser().navigateTo('#/');
+        });
+
+        it('should render home when user navigates to /', function () {
+            expect(element('[ng-view] h1:first').text()).toMatch(/Welcome/);
+        });
+
+    });
+
+    describe('users', function () {
+
+        beforeEach(function () {
+            browser().navigateTo('#/users');
+        });
+
+        it('should render users when user navigates to /users', function () {
+            expect(element('[ng-view] h1:first').text()).toMatch(/Users/);
+        });
+
+    });
+    describe('user', function () {
+
+        beforeEach(function () {
+            browser().navigateTo('#/user/1');
+        });
+
+        it('should render user when user navigates to /user/:id', function () {
+            expect(element('[ng-view] h1:first').text()).toMatch(/Romeo/);
+        });
+
+    });
+});
