e679259c53b091cb162a0b184bf420710e74b794
Radu Cirstoiu <radu.cirstoiu@cegeka.com>
2013-10-15 14:10:28 +0300

    vali, radu: renamed main module

--- ginkgo-seed/spa/test/unit/controllersSpec.js
+++ ginkgo-seed/spa/test/unit/controllersSpec.js
@@ -3,7 +3,7 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function () {
-    beforeEach(module('rechtwijzerAdmin.controllers'));
+    beforeEach(module('userAdmin.controllers'));
 
 
     it('should ....', inject(function () {
