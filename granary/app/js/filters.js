95669c21be58221b36002c59c837c6f644a2ea42
Emilian Losneanu <emilian.losneanu@cegeka.be>
2013-10-15 14:15:41 +0300

    emi: cleanup

--- granary/app/js/filters.js
+++ granary/app/js/filters.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 
-angular.module('rechtwijzerAdmin.filters', []).
+angular.module('granary.filters', []).
     filter('interpolate', ['version', function (version) {
         return function (text) {
             return String(text).replace(/\%VERSION\%/mg, version);
