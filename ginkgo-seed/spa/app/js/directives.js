c25312ff662cbfc4bc965d1b803ccaaabc44dc39
Valentin IONITA <valentin.ionita@cegeka.com>
2013-10-16 15:19:03 +0300

    vali: cleaned up directive

--- ginkgo-seed/spa/app/js/directives.js
+++ ginkgo-seed/spa/app/js/directives.js
@@ -20,15 +20,11 @@ angular.module('userAdmin.directives', []).
                 url : "@"
             },
 
-            controller: ['$http', '$scope', 'LOGIN_REST_URL', function ($http, $scope, LOGIN_REST_URL) {
+            controller: ['$http', '$scope', function ($http, $scope) {
                 $scope.login = function(){
                     $http.post($scope.url ,{username: $scope.username, password: $scope.password});
                 };
-            }],
-
-            link : function(scope, elem, attr, controller){
-            }
-
+            }]
         }
     }
 );
