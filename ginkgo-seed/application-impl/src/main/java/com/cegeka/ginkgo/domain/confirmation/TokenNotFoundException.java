69e45990298a6816f9e9d1b9c1d84e8ad4868664
Radu Cirstoiu <radu.cirstoiu@cegeka.com>
2013-10-16 15:00:59 +0300

    emi, radu: separated code to impl and api

--- ginkgo-seed/application-impl/src/main/java/com/cegeka/ginkgo/domain/confirmation/TokenNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cegeka.ginkgo.domain.confirmation;
-
-public class TokenNotFoundException extends RuntimeException {
-
-    public TokenNotFoundException(String message){
-        super(message);
-    }
-}
--- ginkgo-seed/rest/src/main/java/com/cegeka/ginkgo/domain/confirmation/TokenFactory.java
+++
@@ -1,9 +0,0 @@
-package com.cegeka.ginkgo.domain.confirmation;
-
-import java.util.UUID;
-
-public class TokenFactory {
-    public static String generateToken(){
-        return UUID.randomUUID().toString().replaceAll("-", "");
-    }
-}
