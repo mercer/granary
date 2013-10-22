461e801eab74f3f5b6ee4d7134a352374e02237a
Valentin IONITA <valentin.ionita@cegeka.com>
2013-10-22 15:53:41 +0300

    silviu vali: change login directive when user authenticates

--- ginkgo-seed/spa/test/unit/directivesSpec.js
+++ ginkgo-seed/spa/test/unit/directivesSpec.js
@@ -21,9 +21,17 @@ describe('directives', function () {
     describe('login directive', function () {
         var $compile, scope, template, formElement, compiledElement;
         var loginUrl = 'login-url-parameter';
+        var auth = false;
 
         beforeEach(module('app/directive/login.html'));
-        beforeEach(inject(function ($templateCache, _$compile_, _$rootScope_) {
+        beforeEach(module(function ($provide) {
+            $provide.value('Auth', {
+                isAuthenticated: function () {
+                    return auth;
+                }
+            });
+        }));
+        beforeEach(inject(function ($templateCache, _$compile_, _$rootScope_, $controller, Auth) {
             //assign the template to the expected url called by the directive and put it in the cache
             template = $templateCache.get('app/directive/login.html');
             $templateCache.put('directive/login.html', template);
@@ -34,18 +42,29 @@ describe('directives', function () {
             formElement = angular.element("<login url='" + loginUrl + "' after-login=\"doAfterLogin()\"></login>");
             compiledElement = $compile(formElement)(scope);
             scope = compiledElement.scope();
-            scope.$digest();
+
         }));
 
 
-        it('shuld be compiled to right html', function () {
+        it('should show login fields when user is not authenticated', function () {
+            auth = false;
+            scope.$digest();
 
             expect(compiledElement.attr('url')).toEqual(loginUrl);
             expect(compiledElement.attr('after-login')).toEqual('doAfterLogin()');
-            expect($(compiledElement.children()[0]).attr('ng-model')).toEqual('username');
-            expect($(compiledElement.children()[1]).attr('ng-model')).toEqual('password');
-            expect($(compiledElement.children()[2]).attr('ng-click')).toEqual('login()');
+            expect($("#username", compiledElement).attr('ng-model')).toEqual('username');
+            expect($("#password", compiledElement).attr('ng-model')).toEqual('password');
+            expect($("#login", compiledElement).attr('ng-click')).toEqual('login()');
 
         });
+
+        it('should show greeting message when user is authenticated', inject(function (Auth) {
+            auth = true;
+            scope.$digest();
+
+            expect($(compiledElement.children()[0]).hasClass("ng-hide")).toEqual(true);
+            expect($(compiledElement.children()[1]).hasClass("greeting")).toEqual(true);
+
+        }));
     })
 });
