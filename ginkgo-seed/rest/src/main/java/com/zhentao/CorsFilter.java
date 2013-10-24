74bc562eda048015d25c949fcb983fac5df39922
Radu Cirstoiu <radu.cirstoiu@cegeka.com>
2013-10-24 16:26:14 +0300

    tom, radu: cors: login works, user list works, editing a user doesn't

--- ginkgo-seed/rest/src/main/java/com/zhentao/CorsFilter.java
+++ ginkgo-seed/rest/src/main/java/com/zhentao/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.zhentao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -10,15 +12,21 @@ import java.io.IOException;
 
 public class CorsFilter extends OncePerRequestFilter {
 
+    private static Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+        if ((request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) || (request.getHeader("Origin") != null)) {
             // CORS "pre-flight" request
-            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+            response.addHeader("Access-Control-Allow-Credentials","true");
             response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
             response.addHeader("Access-Control-Allow-Headers", "Content-Type");
             response.addHeader("Access-Control-Max-Age", "1");// 30 min
+
+            logger.info(".........................");
+
         }
         filterChain.doFilter(request, response);
     }
