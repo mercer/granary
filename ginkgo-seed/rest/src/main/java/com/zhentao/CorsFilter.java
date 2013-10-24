63f7a09cce2320fba1ead4f9a470aa4dd2345b84
Radu Cirstoiu <radu.cirstoiu@cegeka.com>
2013-10-24 17:34:48 +0300

    tom, radu: added support for configuring allowed header in web.xml; some refactoring

--- ginkgo-seed/rest/src/main/java/com/zhentao/CorsFilter.java
+++ ginkgo-seed/rest/src/main/java/com/zhentao/CorsFilter.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +15,45 @@ public class CorsFilter extends OncePerRequestFilter {
 
     private static Logger logger = LoggerFactory.getLogger(CorsFilter.class);
 
+    private FilterConfig filterConfig = null;
+    private String allowOrigin;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        if ((request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) || (request.getHeader("Origin") != null)) {
-            // CORS "pre-flight" request
-            response.addHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        if (clientChecksIfPathIsAccessibleViaCors(request)) {
+            replyThatCorsIsEnabled(response);
+            return;
+        }else if(regularCorsCall(request)){
+            response.addHeader("Access-Control-Allow-Origin", allowOrigin);
             response.addHeader("Access-Control-Allow-Credentials","true");
-            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-            response.addHeader("Access-Control-Max-Age", "1");// 30 min
-
-            logger.info(".........................");
-
         }
         filterChain.doFilter(request, response);
+
+    }
+
+    private void replyThatCorsIsEnabled(HttpServletResponse response) {
+        // CORS "pre-flight" request
+        response.addHeader("Access-Control-Allow-Origin", allowOrigin);
+        response.addHeader("Access-Control-Allow-Credentials","true");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.addHeader("Access-Control-Max-Age", "1");// 30 min
+        response.setStatus(200);
+    }
+
+    private boolean regularCorsCall(HttpServletRequest request) {
+        return request.getHeader("Origin") != null;
+    }
+
+    private boolean clientChecksIfPathIsAccessibleViaCors(HttpServletRequest request) {
+        return "OPTIONS".equals(request.getMethod()) && request.getHeader("Access-Control-Request-Method") != null;
+    }
+
+    protected void initFilterBean() throws ServletException {
+        allowOrigin = getFilterConfig().getInitParameter("allow-origin");
+        if(allowOrigin == null){
+            allowOrigin = "*";
+        }
     }
 }
